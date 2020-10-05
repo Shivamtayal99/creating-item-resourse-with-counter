0a9f2160438976c8b0bf0ed6267a460ce7cd25ab
Shivam <tayalshivam99@gmail.com>
2020-10-05 16:00:06 +0530

    optimize code with default dict

--- app.py
+++ app.py
@@ -1,39 +1,29 @@
 from flask import Flask
-from flask_restful import Resource,Api
+from flask_restful import Resource, Api
+from collections import defaultdict
+
+app = Flask(__name__)
+api = Api(app)
+items = defaultdict(int)
 
 
+class Api(Resource):
+    def get(self, name):
+        return items['name']
 
-app=Flask(__name__)
-api=Api(app)
-items=[]
-class Item(Resource):
+    def post(self, name):
+        items['name']+=1
+        return "homepage +1"
+
+
+class Apicount(Resource):
     def get(self,name):
-        for item in items:
-            if item['name']==name:
-                return item
-        return {'item':None}
-
-    def post(self,name):
-        for item in items:
-            if item['name'] == name:
-                item['count']+=1
-                return item
-
-
-        count = 0
-        value = count + 1
-        item={'name':name,'count':value}
-        items.append(item)
-        return item
-
-class ItemList(Resource):
-    def get(self):
-        return {'items':items}
-api.add_resource(Item,'/item/<string:name>')
-api.add_resource(ItemList,'/items')
+        return {'homepage':items['name']}
 
 
 
+api.add_resource(Api, '/api/pixel/<string:name>')
+api.add_resource(Apicount, '/api/count/<string:name>')
 
+app.run(port=5000, debug=True)
 
-app.run(port=5000,debug=True)
