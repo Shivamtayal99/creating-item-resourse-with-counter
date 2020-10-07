733a01757841097c0076eb2a9f3fb0c9f108ebb5
Shivam <tayalshivam99@gmail.com>
2020-10-07 22:43:16 +0530

    optimised the code for retrieving endpoint count from database

--- app.py
+++ app.py
@@ -7,42 +7,32 @@ app = Flask(__name__)
 api = Api(app)
 items = defaultdict(int)
 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="register"
+)
 
 @app.route('/api/pixel/<string:name>',methods=['POST','GET'])
 def increment_counter(name):
     items[name] += 1
-
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="register"
-    )
     mycursor = mydb.cursor()
     if request.method == 'GET':
-
         mycursor.execute("insert into api(`apiname`,`count`)values(%s,1) on duplicate key update `count` = `count`+1",(name,))
 
         mydb.commit()
         mycursor.close()
         return "Ok"
 
-
-
 @app.route('/api/count/<string:name>')
 def get_counter(name):
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="register"
-    )
-
     mycursor = mydb.cursor()
-
     mycursor.execute("SELECT count FROM api where apiname = %s",(name,))
 
     myresult = mycursor.fetchone()
+    if myresult==None:
+        return str(0)
     return str(myresult[0])
 
 
