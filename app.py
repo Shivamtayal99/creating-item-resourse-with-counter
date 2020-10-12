4f425fedc30406e594dfdca7555edf4d49017dfd
Shivam <tayalshivam99@gmail.com>
2020-10-12 14:15:24 +0530

    optimised mysql query

--- app.py
+++ app.py
@@ -1,35 +1,46 @@
-from flask import Flask,request
-from collections import defaultdict
+
+from flask import Flask
 import mysql.connector
 
 app = Flask(__name__)
-items = defaultdict(int)
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="register"
-)
-
-@app.route('/api/pixel/<string:name>',methods=['POST','GET'])
+@app.route('/api/pixel/<string:name>')
 def increment_counter(name):
-    items[name] += 1
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="register"
+    )
     mycursor = mydb.cursor()
-    if request.method == 'GET':
-        mycursor.execute("insert into api(`apiname`,`count`)values(%s,1) on duplicate key update `count` = `count`+1",(name,))
-        mydb.commit()
-        mycursor.close()
-        return "Ok"
+    mycursor.execute("SELECT count FROM app where apiname = %s", (name,))
+    myresult = mycursor.fetchall()
+    if myresult == None:
+        mycursor.execute("insert into app(`apiname`,`count`)values(%s,1)",(name,))
+    else:
+        mycursor.execute("UPDATE app SET count = count + 1 WHERE apiname = %s",(name,))
+    mydb.commit()
+    mycursor.close()
+    return "Ok db"
+
 
 @app.route('/api/count/<string:name>')
 def get_counter(name):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="register"
+    )
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT count FROM api where apiname = %s",(name,))
+    mycursor.execute("SELECT count FROM app where apiname = %s", (name,))
     myresult = mycursor.fetchone()
-    if myresult==None:
+    mydb.commit()
+    mycursor.close()
+    if myresult == None:
         return str(0)
     return str(myresult[0])
+
 
 app.run(port=5000, debug=True)
 
