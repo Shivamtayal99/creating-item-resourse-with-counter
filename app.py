82484936550881616a1698b7fc3cba59c73157b7
Shivam <tayalshivam99@gmail.com>
2020-10-14 11:25:01 +0530

    connection pooling

--- app.py
+++ app.py
@@ -1,46 +1,71 @@
 
 from flask import Flask
 import mysql.connector
+from mysql.connector import Error
+from mysql.connector import pooling
 
 app = Flask(__name__)
 
+
 @app.route('/api/pixel/<string:name>')
 def increment_counter(name):
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="register"
-    )
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT count FROM app where apiname = %s", (name,))
-    myresult = mycursor.fetchall()
-    if myresult == None:
-        mycursor.execute("insert into app(`apiname`,`count`)values(%s,1)",(name,))
-    else:
-        mycursor.execute("UPDATE app SET count = count + 1 WHERE apiname = %s",(name,))
-    mydb.commit()
-    mycursor.close()
-    return "Ok db"
+    try:
+        connection_pool  = mysql.connector.pooling.MySQLConnectionPool(pool_name="pynative_pool",
+                                                                       pool_size=1,
+                                                                       pool_reset_session=False,
+                                                                       host='localhost',
+                                                                       database='register',
+                                                                       user='root',
+                                                                       password='')
+        connection_object = connection_pool.get_connection()
+        if connection_object.is_connected():
+
+
+           cursor = connection_object.cursor()
+           cursor.execute("insert into app(`apiname`,`count`)values(%s,1) on duplicate key update `count` = `count`+1",(name,))
+           cursor.close()
+           connection_object.close()
+           print("MySQL connection is closed")
+
+    except Error as e:
+        print("Error while connecting to MySQL using Connection pool ", e)
+
+
+
+
+
+
+
+    return "okdb"
 
 
 @app.route('/api/count/<string:name>')
 def get_counter(name):
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="register"
-    )
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT count FROM app where apiname = %s", (name,))
-    myresult = mycursor.fetchone()
-    mydb.commit()
-    mycursor.close()
-    if myresult == None:
-        return str(0)
-    return str(myresult[0])
+    try:
+        connection_pool  = mysql.connector.pooling.MySQLConnectionPool(pool_name="pynative_pool",
+                                                                       pool_size=1,
+                                                                       pool_reset_session=True,
+                                                                       host='localhost',
+                                                                       database='register',
+                                                                       user='root',
+                                                                       password='')
+        connection_object = connection_pool.get_connection()
+        if connection_object.is_connected():
 
+
+           cursor = connection_object.cursor()
+           cursor.execute("SELECT count FROM app where apiname = %s", (name,))
+           myresult = cursor.fetchone()
+           cursor.close()
+           connection_object.close()
+
+           if myresult == None:
+               return str(0)
+
+
+           return str(myresult[0])
+    except Error as e:
+        print("Error while connecting to MySQL using Connection pool ", e)
 
 app.run(port=5000, debug=True)
 
