ad275b8e368b1d1869ef4a84a4cc665b6d1ac9f8
Shivam <tayalshivam99@gmail.com>
2020-10-19 14:15:30 +0530

    updated consumer and producer code

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-# import mysql.connector
+import mysql.connector
 from flask import Flask
 from flask import Flask
 from kafka import KafkaProducer
+from kafka import KafkaConsumer
 import json
-# from mysql.connector import pooling
+from mysql.connector import pooling
 
 
 
@@ -15,57 +16,78 @@ producer=KafkaProducer(bootstrap_servers=['192.168.56.1:9092'],value_serializer=
 
 
 
+connection_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="pool",
+                                                              pool_size=12,
+                                                              autocommit=True,
+                                                              pool_reset_session=False,
+                                                              host='localhost',
+                                                              database='register',
+                                                              user='root',
+                                                              password='')
+consumer = KafkaConsumer(
+    "register_api",
+    bootstrap_servers=['192.168.56.1:9092'],
+    auto_offset_reset='earliest',
+    group_id="consumer-group-a")
+
+@app.route('/api/countKafka/<string:name>')
+def read_data(name):
+    connection_object = connection_pool.get_connection()
+    if connection_object.is_connected():
+
+        print("consumer start")
+        for msg in consumer:
+            p = json.loads(msg.value.decode('utf-8'))
+            page=p['page']
+            print('update count for',page)
+            cursor = connection_object.cursor()
+            cursor.execute("insert into app(`apiname`,`count`)values(%s,1) on duplicate key update `count` = `count`+1",
+                           (page,))
+        cursor.close()
+        connection_object.close()
+
+
+
+        return "ok"
+
+
 @app.route('/api/pixelKafka/<string:name>')
 def push_data(name):
-    register_api={"page":name,"count":1}
+    register_api = {"page": name, "count": 1}
     print(register_api)
-    producer.send("register_api",register_api)
+    producer.send("register_api", register_api)
     return "ok kafka"
+@app.route('/api/pixel/<string:name>')
+def increment_counter(name):
 
-# connection_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="pool",
-#                                                               pool_size=12,
-#                                                               autocommit=True,
-#                                                               pool_reset_session=False,
-#                                                               host='localhost',
-#                                                               database='register',
-#                                                               user='root',
-#                                                               password='')
+    connection_object = connection_pool.get_connection()
+    if connection_object.is_connected():
+        print("Connection ID:", connection_object.connection_id)
+        cursor = connection_object.cursor()
+        cursor.execute("insert into app(`apiname`,`count`)values(%s,1) on duplicate key update `count` = `count`+1",
+                         (name,))
+        cursor.close()
+        connection_object.close()
+        print("MySQL connection is closed")
+
+    return "okdb"
 
 
+@app.route('/api/count/<string:name>')
+def get_counter(name):
 
+    connection_object = connection_pool.get_connection()
+    if connection_object.is_connected():
+        cursor = connection_object.cursor()
+        cursor.execute("SELECT count FROM app where apiname = %s", (name,))
+        myresult = cursor.fetchone()
+        cursor.close()
+        connection_object.close()
 
+        if myresult == None:
+            return str(0)
 
-# @app.route('/api/pixel/<string:name>')
-# def increment_counter(name):
-#
-#     connection_object = connection_pool.get_connection()
-#     if connection_object.is_connected():
-#         print("Connection ID:", connection_object.connection_id)
-#         cursor = connection_object.cursor()
-#         cursor.execute("insert into app(`apiname`,`count`)values(%s,1) on duplicate key update `count` = `count`+1",
-#                          (name,))
-#         cursor.close()
-#         connection_object.close()
-#         print("MySQL connection is closed")
-#
-#     return "okdb"
-#
-#
-# @app.route('/api/count/<string:name>')
-# def get_counter(name):
-#
-#     connection_object = connection_pool.get_connection()
-#     if connection_object.is_connected():
-#         cursor = connection_object.cursor()
-#         cursor.execute("SELECT count FROM app where apiname = %s", (name,))
-#         myresult = cursor.fetchone()
-#         cursor.close()
-#         connection_object.close()
-#
-#         if myresult == None:
-#             return str(0)
-#
-#         return str(myresult[0])
+        return str(myresult[0])
 
 
 app.run(port=5000, debug=True)
