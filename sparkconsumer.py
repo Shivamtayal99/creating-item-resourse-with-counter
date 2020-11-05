ff82c04e48a0f2f564fd56836df649a21e6df3ce
Shivam <tayalshivam99@gmail.com>
2020-11-05 16:09:35 +0530

    spark consumer updated to mysql db

--- sparkconsumer.py
+++ sparkconsumer.py
@@ -1,12 +1,8 @@
-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-from kafka import KafkaConsumer
 import mysql.connector
 from mysql.connector import pooling
-
-
 
 connection_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="pool",
                                                               pool_size=12,
@@ -16,6 +12,32 @@ connection_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="pool",
                                                               database='register',
                                                               user='root',
                                                               password='')
+def upsertToDelta(row,epoch_id):
+
+
+    rows=row.select('page','count').collect()
+
+    print(rows)
+    for i in rows:
+        print(i[0])
+        print(i[1])
+        page1=i[0]
+        count1=int(i[1])
+        connection_object = connection_pool.get_connection()
+        if connection_object.is_connected():
+            print("consumer start")
+
+            cursor = connection_object.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS customers (page VARCHAR(10) PRIMARY KEY, count INT)")
+            cursor.execute("insert into customers(`page`,`count`)values(%s,%s) on duplicate key update `count` = `count`+%s",
+                           (page1,count1,count1))
+
+            cursor.close()
+            connection_object.close()
+
+
+
+
 
 # Set Kafka config
 kafka_broker_hostname='192.168.56.1'
@@ -25,6 +47,9 @@ kafka_topic_input='register_api'
 
 
 spark = SparkSession.builder.appName("Api").getOrCreate()
+
+
+
 spark.sparkContext.setLogLevel("WARN")
 df_kafka = spark.readStream \
                 .format("kafka") \
@@ -37,25 +62,19 @@ df_kafka_string = df_kafka.selectExpr("CAST(value AS STRING) as value")
 
 df_schema = StructType() \
                 .add("page", StringType()) \
-                        .add("count", LongType())
+                        .add("count", IntegerType())
 
 df_kafka_string_parsed=df_kafka_string.select(from_json(df_kafka_string.value,df_schema).alias("df_data"))
 
 df_kafka_string_formatted=df_kafka_string_parsed.select(
         col("df_data.page").alias("page"),
         col("df_data.count").alias("count"))
-connection_object = connection_pool.get_connection()
+
 df_agg=df_kafka_string_formatted.groupBy(df_kafka_string_formatted.page).count()
-if connection_object.is_connected():
-    print("consumer start")
 
-    cursor = connection_object.cursor()
-
-    cursor.close()
-    connection_object.close()
 
 
 df_kafka_string_formatted.printSchema()
 df_agg.printSchema()
-q=df_agg.writeStream.outputMode("update").format("console").start()
+q=df_agg.writeStream.outputMode("update").format("delta").foreachBatch(upsertToDelta).start()
 q.awaitTermination()
